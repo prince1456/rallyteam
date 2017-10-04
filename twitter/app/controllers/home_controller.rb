c21fa150d5f1459c11eee386323897f28a86afd1
Ali Akbar Alizada <akbar.alizadeh5@gmail.com>
2017-10-03 22:53:52 -0700

    add random hashtag for default page

--- twitter/app/controllers/home_controller.rb
+++ twitter/app/controllers/home_controller.rb
@@ -1,16 +1,17 @@
 class HomeController < ApplicationController
   def index
+    hashtag = ["love", 'cat', 'dog', 'kiss', 'flower', 'vancouver', 'hiking']
     @fave = Favtweet.new
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key        = 'L4T4aeAjSGPdpZc'
-      config.consumer_secret     = 'VRZK4RAz9z6PoDia4tMLzmNGo4BbWKUoKqqX'
-      config.access_token        = '314qYL9knmJCP7HpIyh6HNk8IhnEAym6cSXk'
-      config.access_token_secret = 'OK9h4Nt0AmeSpWlKbAsuBrRtwm1z'
+      config.consumer_key        = 'fafaf'
+      config.consumer_secret     = 'VRZK4RAz9z6n5awvJPoDia4tMLzmNGo4BbWKUoKqqX'
+      config.access_token        = '314297qYL9knmJCP7HpIyh6HNk8IhnEAym6cSXk'
+      config.access_token_secret = 'OK9h4NtxSvLtlcUW4Fw0AmeSpWlKbAsuBrRtwm1z'
     end
     if params[:search]
       @twitts = client.search("#{params[:search]} -rtfilter%3Alinks&include_entities=1",lang: "en", result_type: params[:result] ).take(params[:number].to_i).collect
   else
-    @twitts = client.search("#dog -rtfilter%3Alinks&include_entities=1", result_type: "recent", lang: "en" ).take(8).collect
+    @twitts = client.search("# #{hashtag.sample} -rtfilter%3Alinks&include_entities=1", result_type: "recent", lang: "en" ).take(8).collect
 
   end
 
